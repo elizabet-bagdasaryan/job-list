1efd92b62b0a0356e1f4783edfd5687efe3f4477
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-04-11 16:33:58 +0400

    "another bug fixing"

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ function displayButtons() {
     chosenOne.remove();
     remove.remove()
     filtered();
-    if(clickedCategories=== 0 ){
+    if(clickedCategories.length === 0 ){
       searchbar.classList.remove("searchbar-active")
     }
   });
